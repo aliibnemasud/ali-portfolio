86f2ab28c5578e59fec6744fc83645ddaba2c4d7
aliibnemasud <mdmasudrony@gmail.com>
2022-09-03 16:49:58 +0600

    add and deleted some images form project

--- src/componants/Home/ContactMe/ContactMe.js
+++ src/componants/Home/ContactMe/ContactMe.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import PrimaryBtn from '../../Shared/PrimaryBtn';
 import './ContactMe.css';
+import contact from '../../img/contact.png';
 
 const ContactMe = () => {
     return (
         <section id='contact' className='my-5 py-5 bg-light mb-5'>
             <div className="container">
-                <h1 className='text-center my-5'>Something about contact!</h1>
-
+                <h1 className='text-center my-3'>Contact Me</h1>
+                <p className='text-center mb-5'>Drop a line. I am ready to help you!</p>
                 <div className="row">
-
                     <div className="col-lg-7 d-flex justify-content-between align-items-center">
-                        <h1 className=''>Drop a line. I am ready to help you!</h1>
+                        <img src={contact} className=" w-50 mx-auto" alt="contact" />                        
                     </div>
-
                     <div className="col-lg-5 shadow forma">
                         <form className='contact-form'>
                             <input className='form-control' placeholder='Your Name' type="text" />
                             <input className='form-control' placeholder='example@mail.com' type="email" />
                             <input className='form-control' placeholder='Budget' type="number" />
-                            <textarea className='form-control' rows="5" placeholder='Project Desicription' type="text" />
-                            
+                            <textarea className='form-control' rows="5" placeholder='Project Desicription' type="text" />                            
                             <PrimaryBtn>Submit</PrimaryBtn>
                         </form>
                     </div>
